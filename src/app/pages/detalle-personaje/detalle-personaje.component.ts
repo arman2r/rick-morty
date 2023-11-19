9583c613ddadb83b58ca17a3828db6b59cd52191
arman2r <arman.2.r@gmail.com>
2023-11-19 15:38:40 -0500

    se agrega carrusel de episodios

--- src/app/pages/detalle-personaje/detalle-personaje.component.ts
+++ src/app/pages/detalle-personaje/detalle-personaje.component.ts
@@ -1,21 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, NgModule, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ICharacter } from 'src/app/models/ICharacter';
 import { GetCharactersService } from 'src/app/services/get-characters.service';
+import { SwiperContainer, register } from 'swiper/element/bundle'; 
+import { SwiperOptions } from 'swiper/types';
+import { SwiperDirective } from '../../directives/swiper.directive';
+
+register();
 
 @Component({
   selector: 'app-detalle-personaje',
   templateUrl: './detalle-personaje.component.html',
-  styleUrls: ['./detalle-personaje.component.scss'],
+  styleUrls: ['./detalle-personaje.component.scss'], 
   standalone: true,
-  imports: [RouterModule, CommonModule]
+  imports: [RouterModule, CommonModule, SwiperDirective],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class DetallePersonajeComponent {
   public id!: number;
   detalle!: ICharacter;
   episodes: any[] = []
-
+  @ViewChild('swiper') swiper!: ElementRef<SwiperContainer>;
+  index = 0
+  swiperConfig: SwiperOptions = { 
+    mousewheel: {
+      forceToAxis: true,
+    },
+    spaceBetween: 10,
+    navigation: true, 
+    breakpoints: {
+      299: {
+        slidesPerView: 1,
+      },
+      360: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    }
+  }
 
   constructor(private getDataService: GetCharactersService, private router: Router, private routerParam: ActivatedRoute){}
 
@@ -47,21 +73,12 @@ export class DetallePersonajeComponent {
           console.log('episodes', getEpisodes[0])
           
         })*/
-
-        const pointerScroll = (elem:any) => {
-
-          let isDrag = false;
-          
-          const toggleDrag = () => isDrag = !isDrag;
-          const drag = (ev:any) => isDrag && (elem.scrollLeft -= ev.movementX);
-          
-          elem.addEventListener("pointerdown", toggleDrag);
-          addEventListener("pointerup", toggleDrag);
-          addEventListener("pointermove", drag);
-        };
-        
-        document.querySelectorAll(".parent").forEach(pointerScroll);
+        this.swiper.nativeElement.swiper.activeIndex = this.index;
+        Object.assign(this.swiper.nativeElement, this.swiperConfig);
+        this.swiper.nativeElement.initialize();
       })
     })
   }
+ 
+
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router'; 
 
-
-@NgModule({
-  declarations: [],
+@NgModule({ 
   imports: [
     CommonModule,
     HomeRoutingModule,
